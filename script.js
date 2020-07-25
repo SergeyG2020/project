67ac1c6e5dfb5f4fc8418d01acbb8cc0227f77a3
Sergey <sergro1974@gmail.com>
2020-07-25 19:46:51 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,58 @@
+'use strict';
+var leftBorderWidth = 1;
+let second = 2;  
+const pi = 3.14159;
+//alert ("Hello world!!!");
+console.log(second);
+
+var num = 25;
+var string = "Hello";
+var sym = Symbol();
+var boolean = true;
+null;
+undefined;
+var obj = {};
+
+console.log (4/0);
+console.log ('string'*2);
+
+var something;
+console.log (something);
+
+let person = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+
+console.log (person.name);
+console.log (person["name"]);
+console.log (person.age);
+
+let array = ['plum.png', 'orang.jpg', 'apple.bmp'];
+
+console.log(array[0]);
+
+//let answer = confirm("Are you hear?");
+
+//console.log(answer);
+
+// let answer = +prompt("Are you 18 year old?", "Yes");
+
+// console.log(typeof(answer));
+// console.log(answer)
+// ;
+
+// console.log("array" + " is object")
+
+let incr = 10,
+    decr = 10;
+
+console.log(incr++);
+console.log(++decr);
+console.log(5%3);
+console.log("2" == 2);
+console.log("2" === 2);
+console.log(incr === 11);
+a = 5;
+console.log(a);
